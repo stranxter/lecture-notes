cfb527d9ef31d84e50a2b8a3b78ee4b7421cc52a
Kalin Georgiev <kalin.georgiev@gmail.com>
2016-11-03 11:47:42 +0200

    Още работа по дърветата

--- samples/03_sdp/2017/01_trees/tressample.cpp
+++ samples/03_sdp/2017/01_trees/tressample.cpp
@@ -60,9 +60,25 @@ private:
 	Node<T>* parseTree (istream &in);
 
 
+	static Node<T>* copyTree (const Node<T> *subTreeRoot);
+
+	void insertBOT (Node<T>*&subTreeRoot,const T& x);
+
+	Node<T>* insertedBOT (Node<T>*subTreeRoot, const T& x);
+
+	void deleteElement (Node<T> *&subTreeRoot, const T&x);
+
+	T minelement (Node<T> *subTreeRoot) const;
+
 public:
 	BTree();
+	BTree (const BTree<T> &other);
+
+	BTree<T>& operator = (const BTree<T> &other);
+
 	BTree<T>& add (const T& data, const char *trace);
+
+	void deleteElement (const T&x);
 
 	void deserialize (istream&);
 
@@ -75,9 +91,200 @@ public:
 
 	void serialize (ostream&)const;
 
+	BTree<T>& insertBOT (const T& x);
+
+	BTree<T> insertedBOT (const T& x);
+
+	T minelement ()const;
+
 	~BTree();
 
 };
+
+template<class T>
+void BTree<T>::deleteElement (Node<T> *&subTreeRoot, const T&x)
+{
+	//триене от празно дърво
+	if (subTreeRoot==NULL)
+		return;
+
+	//триене от листо
+	if (subTreeRoot->data == x &&
+		subTreeRoot->left == NULL &&
+		subTreeRoot->right == NULL)
+
+	{
+		delete subTreeRoot;
+		subTreeRoot = NULL;	
+		return;
+	}
+
+	//триене от лявото поддърво
+	if (x < subTreeRoot->data)
+	{
+		deleteElement (subTreeRoot->left,x);
+		return;
+	}
+
+	//триене от дясното поддърво
+	if (x > subTreeRoot->data)
+	{
+		deleteElement (subTreeRoot->right,x);
+		return;
+	}
+
+	//вече сме сигурни, че трием корена!
+	//освен това сме сигурни, че корена има ПОНЕ ЕДИН
+	//наследник
+
+	//триене на корен само с 1 наследник
+	if (subTreeRoot->right == NULL)
+	{
+		Node<T> *tmp = subTreeRoot;
+		subTreeRoot = subTreeRoot->left;
+		delete tmp;
+		return;
+	}
+
+	//триене на корен само с 1 наследник
+	//този случй може да не се разглежда
+	if (subTreeRoot->left == NULL)
+	{
+		Node<T> *tmp = subTreeRoot;
+		subTreeRoot = subTreeRoot->right;
+		delete tmp;
+		return;
+	}
+
+
+	//триене на корена
+	//вече сме сигурни, че корена има точно два наследника
+	T minel = minelement(subTreeRoot->right);
+	subTreeRoot->data = minel;
+	deleteElement (subTreeRoot->right, minel);
+
+
+}
+
+template<class T>
+void BTree<T>::deleteElement (const T&x)
+{
+	deleteElement (root,x);
+}
+
+template<class T>
+T BTree<T>::minelement (Node<T> *subTreeRoot) const
+{
+	assert (subTreeRoot != NULL);
+	Node<T> *current = subTreeRoot;
+
+	while (current->left != NULL)
+	{
+		current = current->left;
+	}
+
+	return current->data;
+
+}
+
+
+template<class T>
+T BTree<T>::minelement () const
+{
+	return minelement (root);
+
+}
+
+template<class T>
+BTree<T> BTree<T>::insertedBOT (const T& x)
+{
+	BTree<T> result;
+	result.root = insertedBOT (root,x);
+
+	return result;
+}
+
+
+template<class T>
+Node<T>* BTree<T>::insertedBOT (Node<T>*subTreeRoot, const T& x)
+{
+	if (subTreeRoot == NULL)
+	{
+		return new Node<T> (x,NULL,NULL);
+	}
+
+	if (x > subTreeRoot->data)
+	{
+		return new Node<T> (subTreeRoot->data,
+							copyTree(subTreeRoot->left),
+							insertedBOT(subTreeRoot->right,x));
+			                
+	}
+
+	return new Node<T> (subTreeRoot->data,
+						insertedBOT(subTreeRoot->left,x),
+						copyTree(subTreeRoot->right));
+
+}
+
+
+template<class T>
+void BTree<T>::insertBOT (Node<T>* &subTreeRoot,const T& x)
+{
+
+	if (subTreeRoot == NULL)
+	{
+		subTreeRoot = new Node<T> (x,NULL,NULL);
+		return;
+	}
+
+	if (x <= subTreeRoot->data)
+	{
+		insertBOT (subTreeRoot->left,x);
+	} else {
+		insertBOT (subTreeRoot->right,x);
+	}
+
+}
+
+
+template<class T>
+BTree<T>& BTree<T>::insertBOT (const T& x)
+{
+	insertBOT (root,x);	
+	return *this;
+}
+
+template<class T>
+BTree<T>& BTree<T>::operator = (const BTree<T> &other)
+{
+	if (this == &other)
+		return *this;
+
+	deleteAll (root);
+	root = copyTree (other.root);
+
+	return *this;
+}
+
+
+template<class T>
+Node<T>* BTree<T>::copyTree (const Node<T> *subTreeRoot)
+{
+	if (subTreeRoot == NULL)
+		return NULL;
+
+	return new Node<T> (subTreeRoot->data,
+		                copyTree(subTreeRoot->left),
+		                copyTree(subTreeRoot->right));
+}
+
+template<class T>
+BTree<T>::BTree (const BTree<T> &other)
+{
+	root = copyTree (other.root);
+}
+
 
 template<class T>
 void BTree<T>::serialize (Node<T> *subTreeRoot, ostream &out) const
@@ -107,9 +314,7 @@ void BTree<T>::serialize (ostream &out) const
 template<class T>
 void BTree<T>::dottyPrint (ostream &out)
 {
-	out << "digraph G {" << endl;
 	dottyPrint (root,out);
-	out << "}" << endl;
 }
 
 
@@ -198,6 +403,7 @@ template <class T>
 BTree<T>::~BTree()
 {
 	deleteAll (root);
+	root = NULL;
 
 }
 
@@ -248,8 +454,8 @@ void BTree<T>::simplePrint(Node<T> *subTreeRoot) const
 	if (subTreeRoot == NULL)
 		return;
 
-	cout << subTreeRoot->data << " ";
 	simplePrint (subTreeRoot->left);
+	cout << subTreeRoot->data << " ";
 	simplePrint (subTreeRoot->right);
 }
 
@@ -312,33 +518,71 @@ int plusOne (const int& i)
 	return i + 1;
 }
 
-int main ()
+void testAssignment()
 {
-	
-	testMember ();
-
 	BTree<int> t;
 
 	t.add(10,"").add(12,"L").add(14,"R").add(15,"LR");
 	t.simplePrint();
 
-	t.map (plusOne);
+	BTree<int> t1 = t;
+
+	t1.map (plusOne);
+
+	t1 = t;
+
+	assert (t1.member(10) && 
+		    t1.member (12) && 
+		    t1.member (14) && 
+		    t1.member (15));
 
 
-	t.simplePrint ();
-	//t.dottyPrint (cerr);
+}
 
-	cout << "------------" << endl;
-	t.serialize (cout);
+void testMinEl ()
+{
+	BTree<int> t;
+
+	t.insertBOT(59)
+	 .insertBOT(23)
+	 .insertBOT(68)
+	 .insertBOT(190)
+	 .insertBOT(41)
+	 .insertBOT(67);
+
+	 assert (t.minelement() == 23);
+}
+
+int main ()
+{
+	
+	testMember ();
+	testAssignment();
+	testMinEl();
+
+	BTree<int> t;
+
+	t.insertBOT(56)
+	 .insertBOT(23)
+	 .insertBOT(68)
+	 .insertBOT(190)
+	 .insertBOT(41)
+	 .insertBOT(60)
+	 .insertBOT(65)
+	 .insertBOT(59);
 
 
-	BTree<int> test;
-	ifstream in ("data.txt");
+	cerr << "digraph G{" << endl;
+	t.dottyPrint (cerr);
 
-	test.deserialize (in);
-	cout << "---DESERIALIZED TREE-------" << endl;
-	test.serialize(cout);
-	test.dottyPrint (cerr);
+	BTree<int> t1 = t;
+	t1.deleteElement (56);
+
+	t1.dottyPrint(cerr);
+
+	cerr << "}";
+
+	t1.simplePrint ();
 
 	return 0;
 }
