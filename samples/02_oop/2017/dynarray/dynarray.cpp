cf7e472e4eff84b65464bc47f4b331df5666dcfe
Kalin Georgiev <kalin.georgiev@gmail.com>
2017-03-22 15:55:16 +0200

    Лекция за копиране и освобождаване

--- samples/02_oop/2017/dynarray/dynarray.cpp
+++ samples/02_oop/2017/dynarray/dynarray.cpp
@@ -9,7 +9,23 @@ class DynArray
 private:
 	T *array; //addressOfFirstElement
 	size_t size;
+
+	DynArray ()
+	{}
+
 public:
+
+	DynArray (const DynArray<T>& other)
+	{
+		//как да преодолеем това array = other.array?;
+
+		size = other.size;
+		array = new T[size];
+		for (size_t i = 0; i < size; i++)
+		{
+			array[i] = other.array[i];
+		}
+	}
 
 	DynArray (size_t _size)
 	{
@@ -34,13 +50,47 @@ public:
 		cout << "]" << endl;
 	}
 
-	void operator += (DynArray<T>& other)
+	void remove (const T& x)
 	{
-		//this->array == a1.array == [X,X,X,7]
-		//other.array == a2.array == [4,4,4]
-		//result:
-		//this->array == [X,X,X,7,4,4,4]
+		size_t index = 0;
+		while (index < size && array[index] != x)
+		{
+				index++;
+		}
 
+		assert (index < size);
+
+		T *newarray = new T [size-1];
+
+		size_t i;
+		for (i = 0; i < index; i++)//наляво от елемента за изтриване
+		{
+			newarray[i] = array[i];
+		}
+
+		for (i = index+1; i < size; i++)//надясно от елемента за изтриване
+		{
+			newarray[i-1] = array[i];
+		}
+
+		delete array;
+		array = newarray;
+		size--;
+
+	}
+
+	size_t getsize ()
+	{
+		return size;
+	}
+
+	~DynArray ()
+	{
+		delete array;
+	}
+
+	void operator += (const DynArray<T>& other)
+	{
 		T *result = new T [size+other.size];
 		size_t count;
 		for (count = 0; count < size; count++)
@@ -48,17 +98,163 @@ public:
 		for (count = size; count < size+other.size; count++)
 			result[count] = other.array[count-size];
 
-
 		delete array;
 		array = result;
 		size = size + other.size;
 
 	}
+
+
+	DynArray<T> operator + (const DynArray<T>& other)
+	//a1 = a2 + a3; a2.operator + (a3)
+	{
+		/*
+		DynArray result (*this);
+		//result += other;
+		*/
+
+		DynArray result(size+other.size);
+
+		size_t i;
+		for (i = 0; i < size; i++)
+		{
+			result.array[i] = array[i];
+		}
+		for (i = 0; i < other.size; i++)
+		{
+			result.array[size+i] = other.array[i];
+		}
+		//!!!
+
+		return result;
+
+	}
+
+	DynArray<T>& operator = (const DynArray<T>& other)
+	//a3 = a2 = a1;
+	//a3.operator = (a2.operator(a1));
+	{
+
+		if (this != &other)
+		{
+			delete array;
+
+			size = other.size;
+			array = new T[size];
+			for (size_t i = 0; i < size; i++)
+			{
+				array[i] = other.array[i];
+			}
+		}
+
+		//return other;
+		return *this;
+
+	}
+
+	bool operator == (const DynArray<T>& other)
+	{
+		if (size != other.size)
+		{
+			return false;
+		}
+
+		size_t i = 0;
+		while (i < size && array[i] == other.array[i])
+		{
+			i++;
+		}
+
+		return i == size;
+/*
+		for (size_t i = 0; i < size; i++)
+		{
+			if (array[i] != other.array[i])
+				return false;
+		}
+
+		return true;
+*/
+	}
+
+
 };
+
+void testCopy ()
+{
+	DynArray<int> a1(4);
+	a1.operator [](0) = 1;
+	a1[1] = 2;
+	a1[2] = 3;
+	a1[3] = 4;
+
+	DynArray<int> a2 (a1);
+
+	a2[0]= 999;
+	assert (a1[0] == 1);
+
+	DynArray<int> a3  = a1+a2; // a3(a1+a2)
+}
+
+void testRemove ()
+{
+	DynArray<int> a1(4);
+	a1[0] = 1;
+	a1[1] = 2;
+	a1[2] = 3;
+	a1[3] = 4;
+
+
+	a1.remove (1);
+	a1.remove (3);
+
+	assert (a1[0] == 2 && a1[1] == 4);
+	a1.print();
+
+}
+
+void testAssign ()
+{
+	DynArray<int> a1(4);
+	a1[0] = 1;
+	a1[1] = 2;
+	a1[2] = 3;
+	a1[3] = 4;
+
+	DynArray<int> a2(2), a3(2);
+
+	//DynArray<int> a2 = a1;
+	a3 = a2 = a1;
+	//a3.operator = (a2.operator(a1));
+
+	a2[0] = 9;
+	assert (a1[0] == 1);
+
+
+
+}
+
+void testConcat ()
+{
+	DynArray<int> a1(3);
+	a1[0] = 1;
+	a1[1] = 2;
+	a1[2] = 3;
+
+	assert ((a1+a1)[3] == 1);
+
+}
 
 int main ()
 {
-	
+
+	testConcat();
+	testAssign();
+	testRemove();
+	testCopy();
+
+
+
 	//int a1[10];
 	DynArray<int> a1(4);
 
@@ -79,14 +275,14 @@ int main ()
 	a1.print();
 
 
-	/*
-	DynArray<int> a2 = a1;
 
 	a2.print();
 
 	if (a1 == a2)
 		cout << "YES!\n";
 
+
+/*
 	cout << "size of a2" << s2.size() << endl;
 
 	DynArray<int> a3(2);
