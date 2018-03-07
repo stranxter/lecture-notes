e5c6a09bcadc2d74bb2b55787d7107c37402963b
Kalin Georgiev <kalingeorgiev@Alfred.local>
2018-03-07 11:46:59 +0200

    Примери за работа с низове и заделяне на памет

---
+++ samples/02_oop/2018_is/string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+
+
+
+int main ()
+{
+  const char *s1 = "He\0llo ";
+  const char *s2 = "World!";
+
+  //char s3[100];
+  char *s3 = new char [strlen(s1) + strlen(s2) + 1];
+
+  strcpy (s3,s1);
+  strcat (s3,s2);
+
+  std::cout << "r=" << s3 << std::endl;
+
+  strcpy (s3,"Dog!");
+
+  std::cout << "r=" << s3 << std::endl;
+
+  std::cout << "<:" << ("World" < "Dog") << std::endl;
+  std::cout << "<:" << (long)"World" << " " << (long)"Dog" << std::endl;
+  std::cout << "<:" << ("World" == "World") << std::endl;
+
+  std::cout << "addr=" << (long)"World!" << std::endl;
+  std::cout << "addr=" << (long)"World!" << std::endl;
+  std::cout << "addr=" << (long)"World!" << std::endl;
+
+  delete []s3;
+
+}
