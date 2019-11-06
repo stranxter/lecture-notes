25110ba7b396190f9223a59b968767c093bafa7b
Kalin Georgiev <kalingeorgiev@Alfred.local>
2019-11-06 14:54:13 +0200

    Примери от лекции на 06.11

--- samples/01_programming 101/2019-20_kn/arraysandmatr.cpp
+++ samples/01_programming 101/2019-20_kn/arraysandmatr.cpp
@@ -138,26 +138,169 @@ void showHistogram (char s[])
 void findFirstNUniqueWords ()
 {
 
-    const int maxWordSize = 20;
-    const int allWords = 100;
-    const int n = 20;
+    const int maxWordSize = 25;
+    const int allWords = 500;
+    const int n = 50;
 
-    char uniqueWords[n][maxWordSize];
+    char uniqueWords[n][maxWordSize+1];
+    int counters[n] = {0};
     int uniqueWordsSoFar = 0;
 
-    for (int i = 0; i < allWords; ++i)
+    for (int i = 0; i < allWords && uniqueWordsSoFar < n; ++i)
     {
-        char word[maxWordSize];
+        char word[maxWordSize+1];
         cin >> word;
-        for (int j = 0; j < n; ++j)
-        {
-            if (strcmp (uniqueWords[j],word) == 0)
-            {
+        toLowerCase (word);
 
-            }
-        }    
+        int current = 0;
+        while (current < uniqueWordsSoFar &&
+               strcmp (uniqueWords[current],word) != 0)
+        {   //word е различен от първите current на брой
+            //думи в uniqueWords
+            ++current;
+        }
+        if (current == uniqueWordsSoFar)
+        {
+            strcpy (uniqueWords[uniqueWordsSoFar],word);
+            ++uniqueWordsSoFar;
+            counters[current] = 1;
+        } else {
+            ++counters[current];
+        }
+    }
+
+    cout << "Unique words count = " << uniqueWordsSoFar << endl;
+
+    for (int i = 0; i < uniqueWordsSoFar; i++)
+    {
+        cout << "word[" 
+             << i 
+             << "]="
+             << uniqueWords[i]
+             << "("
+             << counters[i]
+             << ")"
+             << endl;
     }
 }
+
+void insertionSortExample ()
+{
+    const int maxElements = 15;
+    int elements[maxElements];
+
+    for (int i = 0; i < maxElements; i++)
+    {
+        /*
+            i: брой въведени и сортирани до 
+               този момент числа
+            elements: първите i елемента на този
+                      масив са сортирани
+        */
+        int newElement;
+        cin >> newElement;
+
+        /*
+            Търсим първото от вече сортираните числа,
+            което е по-голямо от newElement
+        */
+        int desiredIndex = 0;
+        while (desiredIndex < i &&
+               elements[desiredIndex] < newElement)
+        {
+            ++desiredIndex;
+        }
+        //случаят на вмъкване най-вдясно е частен случай,
+        //при който цикъла по-долу ще направи 0 стъпки
+        for (int toMove = i-1; toMove >= desiredIndex; --toMove)
+        {
+            elements[toMove + 1] = elements[toMove];
+        }
+        elements[desiredIndex] = newElement;
+    }
+
+    cout << "====SORTED ARRAY=====\n";
+    printArray (elements,maxElements);
+}
+
+//избутва вдясно елементите на масива 
+//с индекси между from  и to
+//допускаме, че масивът има поне to+1 елемента
+void shiftArray (int a[], int from, int to)
+{
+    for (int toMove = to; toMove >= from; --toMove)
+    {
+        a[toMove+1] = a[toMove];
+    }
+}
+
+int countSmallerThanX (int a[], int size, int x)
+{
+    int count = 0;
+    while (count < size && a[count] < x)
+    {
+        ++count;
+    }
+
+    return count;
+}
+
+void insertionSortExample2()
+{
+    const int maxElements = 5;
+    int elements[maxElements];
+    int newElement;
+
+    for (int i = 0; i < maxElements; i++)
+    {
+        cin >> newElement;
+
+        int desiredIndex = countSmallerThanX (elements,i,newElement);
+        shiftArray (elements,desiredIndex,i-1);
+        elements[desiredIndex] = newElement;
+    }
+
+    cout << "====SORTED ARRAY=====\n";
+    printArray(elements, maxElements);
+}
+
+int findIndexOfMinElement (int a[], int from, int to)
+{
+    int index = from;
+    for (int i = from+1; i <= to; ++i)
+    {
+        if (a[i] < a[index])
+        {
+            index = i;
+        }
+    }
+    return index;
+}
+
+void swap (int a[], int i1, int i2)
+{
+    int save = a[i1];
+    a[i1] = a[i2];
+    a[i2] = save;
+}
+
+void sssort (int a[], int size)
+{
+    for (int i = 0; i < size-1; i++)
+    {
+        /*
+            Всички елементи на масива с индекси по-малки
+            от i са вече подредени по местата си
+        */
+
+       //1. Намираме най-малкия елемент измежду елементите
+       //   с индекси >= i
+       int minel = findIndexOfMinElement (a,i,size-1);
+       swap (a,i,minel);
+
+    }
+}
+
 
 int main ()
 {
@@ -182,13 +325,24 @@ int main ()
     //printArray (result,10);
 
 
-    char s[255];
-    cin.getline (s,254);
+    //char s[255];
+    //cin.getline (s,254);
     
     //toLowerCase (s);
     //cout << s << endl;
 
-    showHistogram (s);
+    //showHistogram (s);
 
+    //findFirstNUniqueWords ();
+
+    //insertionSortExample2();
+
+   int arr[10];
+   inputArray (arr,10);
+
+   sssort(arr,10);
+
+   cout << "====SORTED ARRAY======\n";
+   printArray (arr,10);
 
 }
