ba6a661dce9c3e105405051c8e1bc59252467cba
Kalin Georgiev <kalingeorgiev@Alfred.local>
2021-01-04 21:17:49 +0200

    Пример за лекция 01 по webtech

---
+++ samples/06_webtech/simple-server/server.ts
@@ -0,0 +1,25 @@
+import { connect } from "http2";
+import * as net from "net";
+
+
+function respond(command:string):string
+{
+	if (command=='sayhello\r\n')
+	{
+		return 'Hello!\r\n';
+	} else
+	{
+		return 'Sorry. Don\'t know this command: ' + command;
+	}
+}
+
+function connectionListener(socket:net.Socket):void
+{
+	socket.write('Echo server. Welcome!\r\n');
+	socket.on ('data',function (data){socket.write(respond(data.toString()));});
+
+}
+
+let server:net.Server = net.createServer(connectionListener);
+
+server.listen(1337, '127.0.0.1');
