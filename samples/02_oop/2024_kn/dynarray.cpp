1fad31fe0aa618d1a965eec6380516ed85ddd959
Kalin Georgiev <kalin.georgiev@gmail.com>
2024-03-06 13:08:53 +0200

    dynarray.cpp

--- samples/02_oop/2024_kn/dynarray.cpp
+++ samples/02_oop/2024_kn/dynarray.cpp
@@ -8,13 +8,55 @@ class DynArray
     T *arr;
     size_t size;
 
-    void init()
+    DynArray() //конструктор
     {
         arr = new T[0];
         size = 0;
+
+        std::cout << "I AM BORN!n";
     }
 
-    DynArray& operator+=(T x)
+    DynArray(T x) //конструктор
+    {
+        arr = new T[1];
+        size = 1;
+        arr[0] = x;
+    }
+
+    void copy(const DynArray& other)
+    {
+        this->size = other.size;
+
+        this->arr = new T[this->size];
+
+        for(int i = 0; i < this->size; ++i)
+        {
+            this->arr[i] = other.arr[i];
+        }
+    }
+
+    
+    DynArray(const DynArray& other)
+    {
+        std::cout << "COPY CONSTRUCTOR\n";          
+        copy(other);
+    }
+    
+    DynArray<T>& operator=(const DynArray& other)
+    {
+        //a=b | &a->this, b->other
+
+        if(this != &other)
+        {
+            delete arr;
+            copy(other);
+        }
+
+        //c = (a = b)
+        return *this;
+    }
+
+    DynArray& operator+=(const T& x)
     {
         //arr[size] = x;
         //arr------>[1] [3] [1] [5] +[x]
@@ -47,7 +89,7 @@ class DynArray
         return *this;
     }
 
-    DynArray operator+(DynArray other) const
+    DynArray operator+(const DynArray& other) const
     {
         DynArray result;
         result.init();
@@ -66,7 +108,7 @@ class DynArray
 
     }
 
-    bool operator==(DynArray other)
+    bool operator==(const DynArray& other)
     {
         if (this->size != other.size)
             return false;
@@ -82,10 +124,16 @@ class DynArray
         return true;
     }
 
+    ~DynArray()
+    {
+        std::cout << "DESTRUCTOR!\n";
+        delete arr;
+    }
+
 };
 
 template<typename T>
-std::ostream& operator<<(std::ostream& out, DynArray<T> da)
+std::ostream& operator<<(std::ostream& out, const DynArray<T>& da)
 {
     out << "{";
     for (int i = 0; i < da.size; ++i)
@@ -97,13 +145,52 @@ std::ostream& operator<<(std::ostream& out, DynArray<T> da)
     return out;
 }
 
+void printSize(DynArray<int> arr)
+{
+    std::cout << "size = " << arr.size;
+    arr += 30;
+    std::cout << arr;
+}
+
+
+DynArray<int> createSuperArray()
+{
+    DynArray<int> superArray;
+    superArray += 0; superArray += 0; superArray += 0;
+
+    return superArray;
+}
 
 int main()
 {
+
+    DynArray<int> a,b,c;
+    DynArray<int> newArrayCopyOfOtherArray(a);
+
+    a += 10;
+    a += 20;
+
+    b = a;
+    std::cout << a;
+    std::cout << b;
+
+    a.arr[0] = 40;
+    std::cout << a;
+    std::cout << b;
+
+    DynArray<double> *parray = new DynArray<double>[10];
+    delete []parray;
+    //DynArray<double> arrays[10];
+
+/*
     DynArray<int> a,b;
+    DynArray<int> c(10);
 
-    a.init(); b.init();
+    DynArray<double> *parray = new DynArray<double>[10];
 
+    std::cout << *parray;
+
+    
     std::cout << a;
     a += 1;
     a += 2;
@@ -120,7 +207,7 @@ int main()
     {
         std::cout << "Yes!";
     }
-
+   */
 
     //a = b;
      
