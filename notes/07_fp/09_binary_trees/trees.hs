a1256802ce082461f53b5d3d999dc52ce744f19e
Kalin Georgiev <kalin.georgiev@gmail.com>
2024-11-28 21:59:30 +0200

    Двоични дървета

--- notes/07_fp/09_binary_trees/trees.hs
+++ notes/07_fp/09_binary_trees/trees.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data IntTree = Empty | Node Int IntTree IntTree
   deriving Show
 
@@ -10,3 +12,36 @@ mytree = Node 7
                  (Node 15
                        (Node 70 Empty Empty)
                        Empty))
+
+
+{---Lexical Analysis---}
+
+separators = [' ','\n','\t']
+separator = (flip (elem)) separators
+
+symbols = ['(',')']
+symbol = (flip (elem)) symbols
+
+stopper :: Char -> Bool
+stopper x  = any (\f->f x) [separator,symbol]
+
+next :: String -> Maybe (String, String)
+next [] = Nothing
+next s@(c:cs) | separator c = next cs
+              | symbol c = Just ([c], cs)
+              | otherwise = Just $ break stopper s
+
+lexer = (unfoldr next) . (dropWhile separator)
+
+{-----------------------}
+
+parse :: [String] -> (IntTree, [String])
+parse ("(":")":rest) = (Empty, rest)
+parse ("(":rest) = (Node (read n) l r, rest'')
+  where n = head rest
+        (l, rest') = parse $ tail rest
+        (r, ")":rest'') = parse rest'
+
+tostr :: IntTree -> String
+tostr Empty = "()"
+tostr (Node n l r) = "(" ++ show n ++ " " ++ tostr l ++ " " ++ tostr r ++ ")"
