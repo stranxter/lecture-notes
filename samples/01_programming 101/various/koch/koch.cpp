5cc0eeebaeaf28a491e851660986bef343a56f0c
Kalin Georgiev <kalin.georgiev@gmail.com>
2026-01-12 17:44:13 +0200

    Крива на Кох

---
+++ samples/01_programming 101/various/koch/koch.cpp
@@ -0,0 +1,53 @@
+/*
+Стартов файл за използване на SDL обвиваката paint
+*/
+#include "cpppainter/sdlwrapper.h"
+
+
+void koch(double lenght)
+{
+    if(lenght<10)
+    {
+        paint::turtle::forward(lenght);
+        return;
+    }
+
+    double segment=lenght/3.0;
+
+    koch(segment);
+    paint::turtle::left(60);
+    koch(segment);
+    paint::turtle::right(120);
+    koch(segment);
+    paint::turtle::left(60);
+    koch(segment);
+
+}
+
+void snowflake(double lenght)
+{
+    for(int i=0;i<3;i++)
+    {
+        koch(lenght);
+        paint::turtle::right(120);
+    }
+}
+
+int main()
+{
+    paint::turtle::jump(50,150);
+    paint::turtle::down();
+    //koch(700);
+    //snowflake(400);
+
+    paint::updateGraphics();
+    paint::waitKeypress();
+
+    return 0;
+}
+
+int SDL_main(int argc, char* argv[]) {
+    /* Това е необходимо за SDL под Windows: 
+       библиотекат сменя входната точка на програмата*/
+    return main(); 
+}
